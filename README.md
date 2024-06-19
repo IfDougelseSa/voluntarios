23cf06f5ed74bd67a178fb088f9957347234e1ae
C1328602 DOUGLAS CHRISTOVAO CORTEZ <c1328602@interno.bb.com.br>
2024-06-19 15:45:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+VOLUNTARIOS FRONT
+
+Com nvm
+nvm install v20.12.2
+
+Sem nvm
+https://nodejs.org/en/blog/release/v20.12.2
+
+npm install -g yarn
+
+yarn (instalar dependências)
+
+yarn dev (rodar projeto)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Voluntarios</h1>
+    </>
+  );
+};
+
+export default App;
